430d5b1235c51e07e870f18d2ebfad7bc49e7e08
axelholmgren <axel-eh@hotmail.com>
2023-01-30 15:46:36 +0100

    lab 2

--- Laboration_3/Uppgift_3.py
+++ Laboration_3/Uppgift_3.py
@@ -1,16 +1,16 @@
 from bintreeFile import Bintree
 
 
-svenska = Bintree()
-with open("/Users/axelholmgren/GitHub/DD1320/Laboration_2/word3.txt", "r", encoding = "utf-8") as svenskfil:
-    for rad in svenskfil:
-        ordet = rad.strip()                # Ett trebokstavsord per rad
-        svenska.put(ordet)                 # in i sökträdet
+svenska = Bintree()                                                                                                 # Skapar ett tomt träd för svenska ord
+with open("/Users/axelholmgren/GitHub/DD1320/Laboration_2/word3.txt", "r", encoding = "utf-8") as svenskfil:        # Läser in filen
+    for rad in svenskfil:                                                                                           
+        ordet = rad.strip()                                                                                          # Ett trebokstavsord per rad
+        svenska.put(ordet)                                                                                           # in i sökträdet
 
-engelska = Bintree()
-with open("/Users/axelholmgren/GitHub/DD1320/Laboration_2/engelska.txt", "r", encoding = "utf-8") as engelskafil:
-    for rad in engelskafil:
-        for word in rad.split():
-            if word.strip() in svenska and word.strip() not in engelska:
-                print(word.strip(), end = " ")
-            engelska.put(word.strip())
+engelska = Bintree()                                                                                                 # Skapar ett tomt träd för engelska ord
+with open("/Users/axelholmgren/GitHub/DD1320/Laboration_2/engelska.txt", "r", encoding = "utf-8") as engelskafil:    # Läser in filen
+    for rad in engelskafil: 
+        for word in rad.split():                                                                                     
+            if word.strip() in svenska and word.strip() not in engelska:                                             # kollar om det finns i svenska trädet men inte i engelskaträdet
+                print(word.strip(), end = " ")                                                                       # Skriver ut ordet
+            engelska.put(word.strip())                                                                               # Lägger till i engelskaträdet
--- Laboration_3/bintreeFile.py
+++ Laboration_3/bintreeFile.py
@@ -1,50 +1,57 @@
+# klass skapar ett binärt träd
 class Bintree:
-    def __init__(self):
-        self.root = None
+    def __init__(self):                                 # Ett attribut som är en root, tillhör klassen Node
+        self.root = None 
 
-    def put(self,newvalue):
-        # Sorterar in newvalue i trädet
+    def put(self,newvalue):                             # Metod som lägger in ett nytt värde i trädet, tar newvalue som indata
         self.root = putta(self.root,newvalue)
 
-    def __contains__(self,value):
-        # True om value finns i trädet, False annars
+    def __contains__(self,value):                       # Metod som letar upp ett värde i trädet, tar value som indata
         return finns(self.root,value)
 
-    def write(self):
-        # Skriver ut trädet i inorder
+    def write(self):                                    # Metod som skriver ut trädets värden i inorder (lägst till högst)
         skriv(self.root)
         print("\n")
 
 
+
+# klass som skapar en nod (objekt) med ett värde samt två pekare left, right
 class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-def putta(nod, newvalue):
-    if nod == None:
+
+
+# Fuktion som tar in newvalue som indata och placerar en nod på rätt position i trädet
+def putta(nod, newvalue): 
+    if nod == None:                                     # Om noden inte finns är trädet tomt och den nya noden blir root
         return Node(newvalue)
-    if newvalue == nod.value:   
+    if newvalue == nod.value:                           # Om värdet på noden och det nya värdet är lika returnerar vi noden (lämnar oförändrard)
         return nod
-    if newvalue < nod.value:
+    if newvalue < nod.value:                            # Om det nya värdet är mindre än det värdet i noden, kallar rekursivt och uppdaterar den nya positionen 
         nod.left = putta(nod.left,newvalue)
         return nod
-    if newvalue > nod.value:
+    if newvalue > nod.value:                            # Om det nya värdet är större än det värdet i noden, kallar rekursivt och uppdaterar den nya positionen 
         nod.right = putta(nod.right,newvalue)
         return nod
 
+
+# Denna kod är tagen från Föreläsningsanteckningar 5, DD1320, Sten Andersson 
+# Funktion som tar in ett värde och letar upp om värdet finns
 def finns(nod,value):
-    if nod == None: 
+    if nod == None:                                     # Om noden inte finns finns inget i trädet, returnerar falskt 
         return False
-    if value == nod.value: 
+    if value == nod.value:                              # Om värdet är samma som värdet på noden finns värdet, returnerar falskt
         return True
-    if value < nod.value: 
+    if value < nod.value:                               # Om värdet är mindre än värdet på noden kallas funktionen rekursivt och uppdaterar positionen
         return finns(nod.left,value)
-    if value > nod.value: 
+    if value < nod.value:                               # Om värdet är större än värdet på noden kallas funktionen rekursivt och uppdaterar positionen
+    if value > nod.value:                                
         return finns(nod.right,value)
 
-
+# Funktion som skriver ut trädet i inorder (minst till störst)
 def skriv(nod):
     if nod != None:
         skriv(nod.left)
