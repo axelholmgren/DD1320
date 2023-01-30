a010f8db652e74ba4d6828a996dd5252b47d8da9
axelholmgren <axel-eh@hotmail.com>
2023-01-30 16:03:36 +0100

    l

--- Laboration_3/Uppgift_2.py
+++ Laboration_3/Uppgift_2.py
@@ -1,6 +1,6 @@
 from bintreeFile import Bintree
 svenska = Bintree()
-with open("/Users/axelholmgren/GitHub/DD1320/Laboration_2/word3.txt", "r", encoding = "utf-8") as svenskfil:
+with open("/Users/axelholmgren/GitHub/DD1320/Laboration_3/word3.txt", "r", encoding = "utf-8") as svenskfil:
     for rad in svenskfil:
         ordet = rad.strip()                # Ett trebokstavsord per rad
         if ordet in svenska:
