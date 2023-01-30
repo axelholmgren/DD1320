954ce9f807d757f019d3afa07592182289544983
axelholmgren <axel-eh@hotmail.com>
2023-01-30 15:05:44 +0100

    mappändring

--- Laboration_2/Uppgift_2.py
+++ Laboration_2/Uppgift_2.py
@@ -1,10 +1,22 @@
-from bintreeFile import Bintree
-svenska = Bintree()
-with open("/Users/axelholmgren/GitHub/DD1320/Laboration_2/word3.txt", "r", encoding = "utf-8") as svenskfil:
-    for rad in svenskfil:
-        ordet = rad.strip()                # Ett trebokstavsord per rad
-        if ordet in svenska:
-            print(ordet, end = " ") 
-        else:
-            svenska.put(ordet)             # in i sökträdet
-print("\n")
+from array import array
+
+class ArrayQ:
+    def __init__(self): # konstruktormetod som skapar en array
+        self.__arr = array('b')
+
+    def enqueue(self,value): # Lägger in ett element på sista platsen i arrayen
+        self.__arr.append(value)
+    
+    def dequeue(self): # Tar ut och returnerar det första elementet i arrayen
+        return self.__arr.pop(0)
+
+
+q = ArrayQ()
+q.enqueue(1)
+q.enqueue(2)
+x = q.dequeue()
+y = q.dequeue()
+if (x == 1 and y == 2):
+    print("OK")
+else:
+    print("FAILED")
