0118c9442e641923e6c8853624a2855151eac0b2
axelholmgren <axel-eh@hotmail.com>
2023-02-20 14:43:22 +0100

    make lab5
    original verison of uniqe file

---
+++ Laboration_5/trackFile.py
@@ -0,0 +1,6 @@
+class track:
+    def __init__(self, trackid, låttid, artistnamn, låttitel):
+        self.trackid = trackid
+        self.låttid = låttid
+        self.artistnamn = artistnamn
+        self.låttitel = låttitel
