56083fbbf4e58c652a15578a0bcdcf9af703c002
AntonWallberg <anton.wallberg@outlook.com>
2023-03-31 19:43:52 +0200

    Update rutaclassFile.py

--- Laboration_10/rutaclassFile.py
+++ Laboration_10/rutaclassFile.py
@@ -5,6 +5,7 @@ class Ruta:
     def __init__(self, atom="( )", num=1):
         self.atom = atom
         self.num = num
+        self.weight = 0
         self.next = None
         self.down = None
 
@@ -12,8 +13,7 @@ def main():
     mol = Ruta(atom="Cl", num=2)
     mg = Molgrafik()
     mg.show(mol)
-    while True:
-        pass
+    
 
 if __name__ == '__main__':
     main()
